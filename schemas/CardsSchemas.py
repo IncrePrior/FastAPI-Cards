7f5f161c9e62972f5589dab425274504ad2a9392
babymonkey1111 <babymonkey111@outlook.com>
2023-07-15 21:04:29 +0100

    Validators added for title and text

--- schemas/CardsSchemas.py
+++ schemas/CardsSchemas.py
@@ -4,3 +4,14 @@ class NewCard(BaseModel):
     title: str
     text: str
     
+@validator('title')
+def check_for_max_80_chars(cls, v):
+        if len(v) > 80 : 
+            raise ValueError('Fab that you max out 80 characters for a title. There is more space for you to continue in the note.')
+        return v
+    
+@validator('text')
+def check_for_max_250_chars(cls, v):
+        if len(v) > 250 : 
+            raise ValueError('Great that you max out the 250 characters in this note. If you need more space, continue on a new note.')
+        return v
