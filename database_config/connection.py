cac432d7ef642e33d83a763922ea145ea19c91b7
babymonkey1111 <babymonkey111@outlook.com>
2023-07-16 13:29:29 +0100

    re-entered database URL

--- database_config/connection.py
+++ database_config/connection.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "postgresql://testdb_c0g2_user:jVJZs7Krg3izvJVUAbZRticKW1JWMXdR@dpg-chnr3iik728jv5d6q3q0-a.frankfurt-postgres.render.com/testdb_c0g2"
-
+            
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
