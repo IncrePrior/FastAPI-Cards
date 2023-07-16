4f2a8368d0c574a214912cbf132bde244fc082fc
babymonkey1111 <babymonkey111@outlook.com>
2023-07-16 18:31:59 +0100

    trying to resolve internal server error

--- database_config/models.py
+++ database_config/models.py
@@ -2,16 +2,18 @@ from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Table
 from database_config.connection import Base
 from sqlalchemy.orm import relationship
 
+from schemas import UserSchemas, CardsSchemas
+
 
 class Cards(Base):
     __tablename__ = "cards"
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     text = Column(String)
-    user_id = Column(Integer, ForeignKey(users.id))
+    author_id = Column(Integer, ForeignKey("users.id"))
     author = relationship("Users", back_populates="cards")
-    
-class Users(Base): 
+
+class Users(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
