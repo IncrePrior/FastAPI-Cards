4afcc7389969adc77b3a190448546aa4e1efc169
babymonkey1111 <babymonkey111@outlook.com>
2023-07-16 17:09:34 +0100

    validators added for UserSchemas

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Session
 from schemas.CardsSchemas import Card
 from schemas.UserSchemas import User
 
-models.Base.metadata.create_all(engine)
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
--- schemas/UserSchemas.py
+++ schemas/UserSchemas.py
@@ -5,4 +5,14 @@ class User(BaseModel):
     email: str
     password: str
     
+@validator('name')
+def check_for_max_300_chars(cls, v):
+        if len(v) > 300 : 
+            raise ValueError('Maximum 300-character limit for name.')
+        return v
     
+@validator('password')
+def check_for_min_10_chars(cls, v):
+        if len(v) < 10 : 
+            raise ValueError('Please create a password of at least 10 characters.')
+        return v
