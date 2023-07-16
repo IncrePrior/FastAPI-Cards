2fe3211a5c8eb79bb1774d73051ee477682ee73b
babymonkey1111 <babymonkey111@outlook.com>
2023-07-16 17:59:45 +0100

    just a plain commit

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ You will be able to:
 
 app = FastAPI(
     title = "Cards Against Negativity",
-    description = description
+    description = description,
     summary = "An app to fight self-doubt and negative thoughts.",
     contact = {
         "name" : "Val the OCD coder", 
-        "URL" : "cardsagainstnegativity.com" 
+        "URL" : "cardsagainstnegativity.com",
         "email" : "negativenomore@positivevibesonly.co.uk"}
     )
 
