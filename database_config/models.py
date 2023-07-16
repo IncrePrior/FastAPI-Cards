c0dbac49abe6877783ded19e061abbe1482727cc
babymonkey1111 <babymonkey111@outlook.com>
2023-07-16 18:55:33 +0100

    another commit

--- database_config/models.py
+++ database_config/models.py
@@ -4,6 +4,13 @@ from sqlalchemy.orm import relationship
 
 from schemas import UserSchemas, CardsSchemas
 
+class Users(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String)
+    password = Column(String)
+    cards = relationship("Cards", back_populates="author")
 
 class Cards(Base):
     __tablename__ = "cards"
@@ -13,10 +20,3 @@ class Cards(Base):
     author_id = Column(Integer, ForeignKey("users.id"))
     author = relationship("Users", back_populates="cards")
 
-class Users(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    email = Column(String)
-    password = Column(String)
-    cards = relationship("Cards", back_populates="author")
