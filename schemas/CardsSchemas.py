1425619f6ea739cdc17571919bcde1b3250a1952
babymonkey1111 <babymonkey111@outlook.com>
2023-07-27 19:00:21 +0100

    commit this first

--- schemas/CardsSchemas.py
+++ schemas/CardsSchemas.py
@@ -5,8 +5,8 @@ class Card(BaseModel):
     class Config():
         orm_mode = True
     
-@validator('text')
-def check_for_max_300_chars(cls, v):
-        if len(v) > 300 : 
-            raise ValueError('Great that you max out 300 characters in this note. To continue, start a new note.')
-        return v
+    @validator('text')
+    def check_for_max_300_chars(cls, v):
+            if len(v) > 300 : 
+                raise ValueError('Great that you max out 300 characters in this note. To continue, start a new note.')
+            return v
